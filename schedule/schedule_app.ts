ae02a09f38dfddde18fc3693f491628325a1c768
Drew Brantley <brantley@us.ibm.com>
2018-05-18 11:58:08 -0500

    keep bm alive wed @ 3am add try catch for fail exec

--- schedule/schedule_app.ts
+++ schedule/schedule_app.ts
@@ -16,10 +16,13 @@ function loadScheduledItems(){
 			let {command,rule,params,name,options} = scheduleObj[k];
 			scheduleObj[k].job=schedule.scheduleJob(rule, async ()=>{
 				console.log("running `"+name+"`");
-				let result = await runShell(command,options,params);
+				let result;
+				try{
+					result = await runShell(command,options,params);
+				}catch(e){console.error("schedule app 21");console.error(e);}
 				console.log("`"+name +"` result...");
 				console.log(result);
-			console.log("`"+name+"` next invocation - "+scheduleObj[k].job.nextInvocation());
+				console.log("`"+name+"` next invocation - "+scheduleObj[k].job.nextInvocation());
 			});
 			console.log("`"+name+"` next invocation - "+scheduleObj[k].job.nextInvocation());
 		}catch(e){console.error("schedule 25");console.error(e);}
